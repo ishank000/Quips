55e0c9fe0d5900b523618d0ea25dd835c8123e3f
Ishank Sahni <143020424+ishank000@users.noreply.github.com>
2025-03-01 14:52:31 +0530

    Update settings.py

--- X/settings.py
+++ X/settings.py
@@ -130,8 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join (BASE_DIR,
 'media')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # This is missing
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Ensure this exists
 
 LOGIN_URL = '/accounts/login'
 
